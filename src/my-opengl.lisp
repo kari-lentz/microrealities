c2ceae0c1a4c5692023127d1c7bc4447a8b7e7cd
Kari Lentz <karilentz@att.net>
2012-04-29 22:52:09 -0400

    added basic opengl function objects and scene object, added class-name param to define-tree-class macro

--- src/my-opengl.lisp
+++ src/my-opengl.lisp
@@ -17,7 +17,7 @@
 (defun test-fn(x y z)
   (+ x y z))
 
-(defgeneric render(o))
+(defgeneric call(o))
 
 (defclass test-fo ()
   ((x :initarg :x :reader x :documentation "the x value")
@@ -30,7 +30,7 @@
 (defmethod print-object((o test-fo) stream)
   (format stream (format nil "(test-fo x:~a y:~a z:~a)" (x o) (y o) (z o))))
 
-(defmethod render((o test-fo))
+(defmethod call((o test-fo))
   (+ (x o) (y o) (z o)))
 
 (defun tree-fn(x y z f)
@@ -47,7 +47,7 @@
 
   (defstruct basic-class-member symbol documentation)
 
-  (defun make-declaration(class-name members &rest base-classes)
+  (defun make-declaration(class-name members &optional base-classes)
     `(defclass ,class-name ,base-classes
        ,(loop for member in members 
 	   collecting (create-member (basic-class-member-symbol member) :documentation (basic-class-member-documentation member)))))
@@ -81,6 +81,13 @@
        ,(make-positional-constructor class-name members)
        ,(make-print-method class-name members))))
 
+(defun inspect-basic-class()
+  (macroexpand-1 
+   '(define-basic-class employee 
+       ((name :documentation "name of the employee")
+	(age)
+	(occupation :documentation "occupation for the company")))))
+
 (define-basic-class employee 
     ((name :documentation "name of the employee")
      (age)
@@ -99,32 +106,121 @@
 (defun inspect-function-object()
   (macroexpand-1 '(define-function-object adder-object render (adder x y z))))
 
-(define-function-object adder-object render (adder x y z))
+(define-function-object adder-object call (adder x y z))
 
-(define-basic-class tree 
+(define-basic-class tree-test 
     ((branches)))
 
-(defmethod render((tree tree))
-  (loop for branch in (branches tree)
+(defmethod call((tree-test tree-test))
+  (loop for branch in (branches tree-test)
        collecting
-       (render branch)))
+       (call branch)))
 
-(defmacro define-tree-class(class-name (&rest member-specs))
+(defmacro define-tree-class(class-name (&rest base-classes) (&rest member-specs))
   (let ((members (loop for member-spec in member-specs collecting (apply #'make-basic-class-member :symbol member-spec))))
     (let ((members+inheritted (concatenate 'list members (list (make-basic-class-member :symbol 'branches)))))
       `(progn
-	 ,(make-declaration class-name members 'tree)
+	 ,(make-declaration class-name members base-classes)
 	 ,(make-positional-constructor class-name members+inheritted t)
 	 ,(make-print-method class-name members+inheritted)))))
 
 (defun inspect-tree-object()
-  (macroexpand-1 '(define-tree-class multiplier 
+  (macroexpand-1 '(define-tree-class multiplier (tree-test) 
 		   ((value-1)
 		    (value-2)))))
 
-(define-tree-class multiplier 
+(define-tree-class multiplier (tree-test)
     ((value-1)
      (value-2)))
 
-(defmethod render((multiplier multiplier))
+(defmethod call((multiplier multiplier))
   (* (value-1 multiplier) (value-2 multiplier) (apply #'+ (call-next-method))))
+
+(define-basic-class gl-tree 
+    ((branches)))
+
+(defmethod render((gl-tree gl-tree))
+  (loop for branch in (branches gl-tree)
+       do
+       (render branch)))
+
+(defgeneric render (gl-object))
+
+(define-function-object vertex render (gl:vertex x y z))
+(define-function-object color render (gl:color r g b)) 
+(define-function-object normal render (gl:normal x y z))
+(define-function-object translate render (gl:translate x y z))
+(define-function-object rotate render (gl:rotate theta x y z))
+
+(define-tree-class triangles (gl-tree)())
+
+(defmethod render((triangles triangles))
+  (gl:with-primitive :triangles
+    (call-next-method)))
+
+(define-tree-class quads (gl-tree)())
+
+(defmethod render((quads quads))
+  (gl:with-primitive :quads
+    (call-next-method)))
+
+(define-tree-class pushed-matrix (gl-tree)())
+
+(defmethod render((pushed-matrix pushed-matrix))
+  (gl:with-pushed-matrix
+    (call-next-method)))
+
+(define-tree-class scene (gl-tree) 
+    ((field-of-view)
+     (min-z)
+     (max-z)
+     (viewport-width)
+     (viewport-height)))
+
+(defmethod render((scene scene))
+  (sdl:with-init ()
+    (sdl:window (viewport-width scene) (viewport-height scene) :flags sdl:sdl-opengl)
+    ;; cl-opengl needs platform specific support to be able to load GL
+    ;; extensions, so we need to tell it how to do so in lispbuilder-sdl
+    (setf cl-opengl-bindings:*gl-get-proc-address* #'sdl-cffi::sdl-gl-get-proc-address)
+    (gl:viewport 0 0 (viewport-width scene) (viewport-height scene))
+    (sdl:enable-key-repeat 500 150)
+
+    (gl::matrix-mode :projection)
+    (gl:load-identity)
+
+    (let ((width (* (tan (field-of-view scene)) (min-z scene)))) 
+      (let ((height (* *aspect* width)))
+	(gl:frustum (- 0.0d0 width) width (- 0.0d0 height) height (min-z scene) (max-z scene))))
+
+    (sdl:with-events ()
+      (:quit-event () t)
+      (:idle ()
+	     ;; this lets slime keep working while the main loop is running
+	     ;; in sbcl using the :fd-handler swank:*communication-style*
+	     ;; (something similar might help in some other lisps, not sure which though)
+	     #+(and sbcl (not sb-thread)) (restartable
+					   (sb-sys:serve-all-events 0))
+					;(restartable (draw))))))
+
+	     ;(gl:enable :texture-2d)
+	     (gl:enable :cull-face :lighting :light0 :depth-test :normalize :color-material)
+	     
+	     (gl:clear :color-buffer-bit :depth-buffer-bit)
+	     (gl:cull-face :back)
+
+	     (gl::matrix-mode :modelview)
+	     (gl:load-identity)
+	     (call-next-method)
+	     (gl:flush)
+	     (sdl:update-display)))))
+
+(defun display-scene()
+  
+  (let ((w 50)(h 50) (z 0))
+    (render 
+     (scene (* PI (/ 5 18)) 1 100 640 480 
+	    (translate 0 0 -75)
+	    (rotate 30 0 1 0)
+	    (quads (color 1 0 0) (vertex (- w) h (- z)) (color 0.5 0.5 0) (vertex (- w) (- h) (- z)) (color 0.0 0.5 0) (vertex w (- h) (- z)) (color 0.0 1.0 0) (vertex w h (- z))))))) 
+
