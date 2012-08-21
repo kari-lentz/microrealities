31d630039e8c746448f75488571db87075deb046
Kari Lentz <karilentz@att.net>
2012-08-20 22:42:44 -0400

    macro implementation of with-scene and triangle fan tests

--- my-opengl/src/my-opengl.lisp
+++ my-opengl/src/my-opengl.lisp
@@ -1,226 +1,156 @@
 (defpackage :my-opengl
   (:documentation "opengl wrapper")
-  (:use :cl :my-env :utility)
+  (:use :cl :my-env :utility :gl)
   (:export :run))
 
 (in-package :my-opengl)
 
-(defparameter *aspect* 1.0)
+(defconstant +aspect+ 1.0)
 (defconstant +TWO-PI+ (* 2 PI))
 (defconstant +HALF-PI+ (* 0.5 PI))
 
-(defmacro with-gensyms( ( &rest symbol-names ) &body forms)
-  `(let (,@(loop for symbol-name in symbol-names collecting
-	       `(,symbol-name (gensym))))
-     ,@forms))
+(defun degrees(num-degrees)
+  (* (/ num-degrees 180) PI))
 
-(defun test-fn(x y z)
-  (+ x y z))
+(defun spherical-to-cartesian(rad alt az)
+  (let ((sin-alt (sin alt))) 
+    (values
+     (* rad sin-alt (cos az))
+     (* rad sin-alt (sin az))
+     (* rad (cos alt)))))
 
-(defgeneric call(o))
+(defmacro from-spherical((x-var y-var z-var) radius alt-ang az-ang &body body) 
+ ` (multiple-value-bind (,x-var ,y-var ,z-var) 
+       (spherical-to-cartesian ,radius ,alt-ang ,az-ang)  
+     ,@body))
 
-(defclass test-fo ()
-  ((x :initarg :x :reader x :documentation "the x value")
-   (y :initarg :y :reader y)
-   (z :initarg :z :reader z)))
- 
-(defun test-fo(x y z)
-  (make-instance 'test-fo :x x :y y :z z))
+(defun normalize(x y z)
+  (let ((anti-mag (/ 1 (sqrt (+ (* x x) (* y y) (* z z))))))
+    (values (* x anti-mag) (* y anti-mag) (* z anti-mag))))
 
-(defmethod print-object((o test-fo) stream)
-  (format stream (format nil "(test-fo x:~a y:~a z:~a)" (x o) (y o) (z o))))
+(defmacro with-normalized((x-var y-var z-var) x y z &body body)
+  ` (multiple-value-bind (,x-var ,y-var ,z-var) 
+	(normalize ,x ,y ,z)  
+      ,@body))
 
-(defmethod call((o test-fo))
-  (+ (x o) (y o) (z o)))
+(defmacro with-triangle(&body body)
+  `(gl:with-primitives :triangles ,@body))
 
-(defun tree-fn(x y z f)
-  (let ((v (funcall f (* 10 x) (* 10 y) (* 10 z))))
-    (mapcar (lambda(x) (/ x 10)) v)))
+(defmacro with-quad-strip(&body body)
+  `(gl:with-primitives :quad-strip ,@body))
 
-(eval-when (:compile-toplevel :load-toplevel :execute) 
-   
-  (defun create-member(symbol &key documentation)
-    (concatenate 
-     'list 
-     `(,symbol :initarg ,(to-keyword symbol) :reader ,symbol) 
-     (and documentation (list :documentation documentation))))
+(defmacro with-triangle-strip(&body body)
+  `(gl:with-primitives :triangle-strip ,@body))
 
-  (defstruct basic-class-member symbol documentation)
+(defmacro with-triangle-fan(&body body)
+  `(gl:with-primitives :triangle-fan ,@body))
 
-  (defun make-declaration(class-name members &optional base-classes)
-    `(defclass ,class-name ,base-classes
-       ,(loop for member in members 
-	   collecting (create-member (basic-class-member-symbol member) :documentation (basic-class-member-documentation member)))))
+(defmacro with-quads(&body body)
+  `(gl:with-primitives :quads ,@body))
 
-  (defun make-positional-constructor(class-name members &optional last-rest-p)
-    (flet ((pass-rest(params)
-	     (if last-rest-p
-		 (concatenate 'list (butlast params) (cons '&rest (last params)))
-		 params)))
+(defmacro assign-light(index x y z w)
+    `(light ,(to-keyword (.sym 'light index)) :position (vector ,x ,y ,z ,w)))
+
+(defmacro with-scene((field-of-view min-z max-z &optional (viewport-width 640) (viewport-height 480)) &body frms)
+  (with-gensyms (width height)  
+    (with-once-only (field-of-view min-z max-z viewport-width viewport-height)
+      `(sdl:with-init ()
+	 (sdl:window ,viewport-width ,viewport-height :flags sdl:sdl-opengl)
+	 ;; cl-opengl needs platform specific support to be able to load GL
+	 ;; extensions, so we need to tell it how to do so in lispbuilder-sdl
+	 (setf cl-opengl-bindings:*gl-get-proc-address* #'sdl-cffi::sdl-gl-get-proc-address)
+	 (gl:viewport 0 0 ,viewport-width ,viewport-height)
+	 (sdl:enable-key-repeat 500 150)
       
-      `(defun ,class-name 
-	   ,(pass-rest (loop for member in members collecting (basic-class-member-symbol member)))
-	 (make-instance (quote ,class-name) 
-			,@(apply #'concatenate 'list (loop for member in members collecting `(,(to-keyword (basic-class-member-symbol member)) ,(basic-class-member-symbol member)))))))) 
-    
-    (defun make-print-method(class-name members)
-      `(defmethod print-object((,class-name ,class-name) stream)
-	 (format stream 
-		 (format nil 
-			 ,(format nil "(~a ~a)" class-name 
-				  (join " " 
-					(loop for member in members collecting 
-					     (format nil "~a:~a" (basic-class-member-symbol member) "~a"))))
-			 ,@(loop for member in members collecting
-				`(,(basic-class-member-symbol member) ,class-name)))))))
-
-(defmacro define-basic-class(class-name (&rest member-specs))
-  (let ((members (loop for member-spec in member-specs collecting (apply #'make-basic-class-member :symbol member-spec))))
-    `(progn
-       ,(make-declaration class-name members)
-       ,(make-positional-constructor class-name members)
-       ,(make-print-method class-name members))))
-
-(defun inspect-basic-class()
-  (macroexpand-1 
-   '(define-basic-class employee 
-       ((name :documentation "name of the employee")
-	(age)
-	(occupation :documentation "occupation for the company")))))
-
-(define-basic-class employee 
-    ((name :documentation "name of the employee")
-     (age)
-     (occupation :documentation "occupation for the company")))
-
-(defmacro define-function-object(function-object-name call-method-name (function-name &rest args))
-  `(progn
-     (define-basic-class ,function-object-name
-	 ,(loop for arg in args collecting `(,arg)))
-     (defmethod ,call-method-name((,function-object-name ,function-object-name))
-       (,function-name ,@(loop for arg in args collecting `(,arg  ,function-object-name))))))
-	 
-(defun adder(x y z)
-  (+ x y z))
-
-(defun inspect-function-object()
-  (macroexpand-1 '(define-function-object adder-object render (adder x y z))))
-
-(define-function-object adder-object call (adder x y z))
-
-(define-basic-class tree-test 
-    ((branches)))
-
-(defmethod call((tree-test tree-test))
-  (loop for branch in (branches tree-test)
-       collecting
-       (call branch)))
-
-(defmacro define-tree-class(class-name (&rest base-classes) (&rest member-specs))
-  (let ((members (loop for member-spec in member-specs collecting (apply #'make-basic-class-member :symbol member-spec))))
-    (let ((members+inheritted (concatenate 'list members (list (make-basic-class-member :symbol 'branches)))))
-      `(progn
-	 ,(make-declaration class-name members base-classes)
-	 ,(make-positional-constructor class-name members+inheritted t)
-	 ,(make-print-method class-name members+inheritted)))))
-
-(defun inspect-tree-object()
-  (macroexpand-1 '(define-tree-class multiplier (tree-test) 
-		   ((value-1)
-		    (value-2)))))
-
-(define-tree-class multiplier (tree-test)
-    ((value-1)
-     (value-2)))
-
-(defmethod call((multiplier multiplier))
-  (* (value-1 multiplier) (value-2 multiplier) (apply #'+ (call-next-method))))
-
-(define-basic-class gl-tree 
-    ((branches)))
-
-(defmethod render((gl-tree gl-tree))
-  (loop for branch in (branches gl-tree)
-       do
-       (render branch)))
-
-(defgeneric render (gl-object))
-
-(define-function-object vertex render (gl:vertex x y z))
-(define-function-object color render (gl:color r g b)) 
-(define-function-object normal render (gl:normal x y z))
-(define-function-object translate render (gl:translate x y z))
-(define-function-object rotate render (gl:rotate theta x y z))
-
-(define-tree-class triangles (gl-tree)())
-
-(defmethod render((triangles triangles))
-  (gl:with-primitive :triangles
-    (call-next-method)))
-
-(define-tree-class quads (gl-tree)())
-
-(defmethod render((quads quads))
-  (gl:with-primitive :quads
-    (call-next-method)))
-
-(define-tree-class pushed-matrix (gl-tree)())
-
-(defmethod render((pushed-matrix pushed-matrix))
-  (gl:with-pushed-matrix
-    (call-next-method)))
-
-(define-tree-class scene (gl-tree) 
-    ((field-of-view)
-     (min-z)
-     (max-z)
-     (viewport-width)
-     (viewport-height)))
-
-(defmethod render((scene scene))
-  (sdl:with-init ()
-    (sdl:window (viewport-width scene) (viewport-height scene) :flags sdl:sdl-opengl)
-    ;; cl-opengl needs platform specific support to be able to load GL
-    ;; extensions, so we need to tell it how to do so in lispbuilder-sdl
-    (setf cl-opengl-bindings:*gl-get-proc-address* #'sdl-cffi::sdl-gl-get-proc-address)
-    (gl:viewport 0 0 (viewport-width scene) (viewport-height scene))
-    (sdl:enable-key-repeat 500 150)
-
-    (gl::matrix-mode :projection)
-    (gl:load-identity)
-
-    (let ((width (* (tan (field-of-view scene)) (min-z scene)))) 
-      (let ((height (* *aspect* width)))
-	(gl:frustum (- 0.0d0 width) width (- 0.0d0 height) height (min-z scene) (max-z scene))))
-
-    (sdl:with-events ()
-      (:quit-event () t)
-      (:idle ()
-	     ;; this lets slime keep working while the main loop is running
-	     ;; in sbcl using the :fd-handler swank:*communication-style*
-	     ;; (something similar might help in some other lisps, not sure which though)
-	     #+(and sbcl (not sb-thread)) (restartable
-					   (sb-sys:serve-all-events 0))
+	 (gl::matrix-mode :projection)
+	 (gl:load-identity)
+      
+	 (let ((,width (* (tan ,field-of-view) ,min-z))) 
+	   (let ((,height (* +aspect+ ,width)))
+	     (gl:frustum (- 0.0d0 ,width) ,width (- 0.0d0 ,height) ,height ,min-z ,max-z)))
+       
+	 (sdl:with-events ()
+	   (:quit-event () t)
+	   (:idle ()
+		  ;; this lets slime keep working while the main loop is running
+		  ;; in sbcl using the :fd-handler swank:*communication-style*
+		  ;; (something similar might help in some other lisps, not sure which though)
+		  #+(and sbcl (not sb-thread)) (restartable
+					      (sb-sys:serve-all-events 0))
 					;(restartable (draw))))))
-
-	     ;(gl:enable :texture-2d)
-	     (gl:enable :cull-face :lighting :light0 :depth-test :normalize :color-material)
+		
+					;(gl:enable :texture-2d)
+		  (gl:enable :cull-face :lighting :light0 :depth-test :normalize :color-material)
 	     
-	     (gl:clear :color-buffer-bit :depth-buffer-bit)
-	     (gl:cull-face :back)
+		  (gl:clear :color-buffer-bit :depth-buffer-bit)
+		  (gl:cull-face :back)
+	     
+		  (gl::matrix-mode :modelview)
+		  (gl:load-identity)
+		  ,@frms
+		  (gl:flush)
+		  (sdl:update-display)))))))
 
-	     (gl::matrix-mode :modelview)
-	     (gl:load-identity)
-	     (call-next-method)
-	     (gl:flush)
-	     (sdl:update-display)))))
-
-(defun display-scene()
-  
+(defun display-scene-quad()  
   (let ((w 50)(h 50) (z 0))
-    (render 
-     (scene (* PI (/ 5 18)) 1 100 640 480 
-	    (translate 0 0 -75)
-	    (rotate 30 0 1 0)
-	    (quads (color 1 0 0) (vertex (- w) h (- z)) (color 0.5 0.5 0) (vertex (- w) (- h) (- z)) (color 0.0 0.5 0) (vertex w (- h) (- z)) (color 0.0 1.0 0) (vertex w h (- z))))))) 
+     (with-scene ((degrees 60) 1 100 640 480)
+       (assign-light 0 1 0 0 0)
+       (color-material :front :ambient-and-diffuse)
+       
+       (translate 0 0 -75)
+       (rotate 30 0 1 0)
+       (with-quads (color 1 0 0) (vertex (- w) h (- z)) (color 0.5 0.5 0) (vertex (- w) (- h) (- z)) (color 0.0 0.5 0) (vertex w (- h) (- z)) (color 0.0 1.0 0) (vertex w h (- z))))))
 
+(defun umbrella-points(radius slices)
+  (let ((ang-inc (/ +TWO-PI+ slices)))
+    (append
+     (list 
+      (list 0 0 (/ radius 1))) 
+     (map-range (ang (1+ slices)) (from-spherical (x y z)  radius (degrees 30) (* ang ang-inc) 
+			       (list x y z))))))
+
+(defun display-scene-triangle-fan()
+  
+     (with-scene ((degrees 60) 1 100 640 480)
+
+       (assign-light 0 1 0 0 0)
+       (translate 0 0 -75)
+       (color-material :front :ambient-and-diffuse)
+       (color 0 1 0)
+
+       (with-triangle-fan 
+	 (loop for (x y z) in (umbrella-points 50 16) do
+	      (with-normalized (x y z) x y z
+		  (normal x y z))
+	      (vertex x y z)))))
+
+(defun display-scene-triangle()
+  
+     (with-scene ((degrees 60) 1 100 640 480)
+
+       (assign-light 0 0 1 0 0)
+       (translate 0 0 -75)
+       (color-material :front :ambient-and-diffuse)
+       (color 0 1 0)
+       (with-triangle-fan
+	 (vertex 0 0 50)
+	 (vertex 35 0 35)
+	 (vertex 0 35 35)
+	 (vertex -35 35 35))))
+
+;; (defun display-scene-triangle-fan()
+  
+;;      (with-scene ((degrees 60) 1 100 640 480)
+
+;;        (assign-light 0 1 0 0 0)
+;;        (translate 0 0 -75)
+;;        (color-material :front :ambient-and-diffuse)
+;;        (color 0 1 0)
+
+;;        (with-quad-strip 
+;; 	 (let ((radius 50)(slices 16))
+;; 	   (let ((delta-ang (/ +TWO-PI+ slices)))
+;; 	     (map-range (n slices)
+;; 			(from-spherical (x y z) radius (degrees 30) (* n delta-ang)
+;; 			  (vertex x y z)
+;; 			  (vertex x y z)
