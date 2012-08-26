e9083a1d8e9f3156322a26f5d365e70721ab14d1
Kari Lentz <karilentz@att.net>
2012-08-25 21:04:16 -0400

    compile-time macro to draw sphere points

--- my-opengl/src/my-opengl.lisp
+++ my-opengl/src/my-opengl.lisp
@@ -1,7 +1,8 @@
 (defpackage :my-opengl
   (:documentation "opengl wrapper")
   (:use :cl :my-env :utility :gl)
-  (:export :run))
+  (:export :run
+	   #:x))
 
 (in-package :my-opengl)
 
@@ -9,24 +10,27 @@
 (defconstant +TWO-PI+ (* 2 PI))
 (defconstant +HALF-PI+ (* 0.5 PI))
 
-(defun degrees(num-degrees)
-  (* (/ num-degrees 180) PI))
+(with-full-eval
 
-(defun spherical-to-cartesian(rad alt az)
-  (let ((sin-alt (sin alt))) 
-    (values
-     (* rad sin-alt (cos az))
-     (* rad sin-alt (sin az))
-     (* rad (cos alt)))))
+  (defun degrees(num-degrees)
+    (* (/ num-degrees 180) PI))
+
+  (defun spherical-to-cartesian(rad alt az)
+    (let ((sin-alt (sin alt))) 
+      (values
+       (* rad sin-alt (cos az))
+       (* rad sin-alt (sin az))
+       (* rad (cos alt))))))
 
 (defmacro from-spherical((x-var y-var z-var radius alt-ang az-ang) &body body) 
  ` (multiple-value-bind (,x-var ,y-var ,z-var) 
        (spherical-to-cartesian ,radius ,alt-ang ,az-ang)  
      ,@body))
 
-(defun normalize(x y z)
-  (let ((anti-mag (/ 1 (sqrt (+ (* x x) (* y y) (* z z))))))
-    (values (* x anti-mag) (* y anti-mag) (* z anti-mag))))
+(with-full-eval
+  (defun normalize(x y z)
+    (let ((anti-mag (/ 1 (sqrt (+ (* x x) (* y y) (* z z))))))
+      (values (* x anti-mag) (* y anti-mag) (* z anti-mag)))))
 
 (defmacro with-normalized((x-var y-var z-var x y z) &body body)
   ` (multiple-value-bind (,x-var ,y-var ,z-var) 
@@ -157,4 +161,78 @@
 	       (for-each-range (n (1+ slices))
 		 (make-point (degrees 15) (* n delta-ang))
 		 (make-point (degrees 30) (* n delta-ang)))))))))			  
-				      
+		
+(with-full-eval		      
+  (defstruct 3d-vector x y z)
+
+  (defmethod make-load-form ((self 3d-vector) &optional environment)
+    (declare (ignore environment))
+    ;; Note that this definition only works because X and Y do not
+    ;; contain information which refers back to the object itself.
+    ;; For a more general solution to this problem, see revised example below.
+    `(make-3d-vector :x ',(3d-vector-x self) :y ',(3d-vector-y self) :z ',(3d-vector-z self))))
+
+(defmacro make-globe-points(radius slices)
+
+  ; performs compile time trig calculations for vertex of a sphere
+  ; slices is a compiled constant, radius is 1.0 at compile and mulitplied out at run-time 
+  ;
+
+  (let ((slices (eval slices)))
+    (unless (numberp slices) (error "in make-globe-points slices needs to evaluate to a literal decimal number - compile time macro"))  
+    (let ((width (1+ slices))(height (1- (/ slices 2))))
+      (let ((grid (make-array (* width height ) :element-type '3d-vector :initial-element (make-3d-vector :x 0 :y 0 :z 0)))
+	    (delta (/ +TWO-PI+ slices)))
+	(for-each-range 
+	    (j height)
+	  (for-each-range 
+	      (i width)
+	    (from-spherical (x y z 1.0d0 (* (1+ j) delta) (* i delta))
+	      (setf (aref grid (+ i (* width j))) (make-3d-vector :x x :y y :z z)))))
+	(with-gensyms (x y v)
+	  `(lambda(,x ,y)
+	     (unless (and (>= ,x 0)(< ,x ,width)) (error (format nil "make-globe-points: range error x needs to be from 0 to ~a" ,(1- width))))
+	     (unless (and (>= ,y 0)(< ,y ,height)) (error (format nil "make-globe-points: range error y needs to be from 0 to ~a" ,(1- height))))
+	     (with-slots (x y z) (aref ,grid (+ ,x (* ,y ,width)))
+	       (values-list (qmap (,v) (* ,v ,radius)(list x y z))))))))))
+
+(defconstant +slices+ 16)
+
+(defun draw-globe(radius)
+  (let ((fpoints (make-globe-points radius +slices+))(x-slices (1+ +slices+))(y-slices (1- (/ +slices+ 2))))
+					;top triangle fan
+    (with-triangle-fan
+      (vertex 0 0 radius)
+      (for-each-range (n x-slices)
+	(multiple-value-bind (x y z)(funcall fpoints n 0)
+	  (normal x y z)
+	  (vertex x y z))))
+
+    (with-quad-strip
+      (for-each-range (n (1- y-slices))
+	(for-each-range (m x-slices)
+	  (progn
+	    (multiple-value-bind (x y z)(funcall fpoints m n)
+	      (normal x y z)
+	      (vertex x y z))
+	    (multiple-value-bind (x y z)(funcall fpoints m (1+ n))
+	      (normal x y z)
+	      (vertex x y z))))))
+
+    (with-triangle-fan
+      (vertex 0 0 (- radius))
+      (for-each-range (n x-slices)
+	(multiple-value-bind (x y z)(funcall fpoints (- x-slices n 1) (1- y-slices))
+	  (vertex x y z))))))
+
+(defun display-globe()
+
+  (with-scene ((degrees 60) 1 100 640 480)
+
+    (assign-light 0 1 0 0 0)
+    (translate 0 0 -75)
+    (color-material :front :ambient-and-diffuse)
+    (color 0 1 0)
+    (draw-globe 50.0)))
+
+
