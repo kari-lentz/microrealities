70cc8ddff6d63c9ccf92cac9865925a8fd548b1e
Kari Lentz <karilentz@att.net>
2012-10-08 23:57:02 -0400

    utility.lisp defeasyclass supports export-all keyword to allow auto export of all class members by default

--- utility/src/utility.lisp
+++ utility/src/utility.lisp
@@ -496,10 +496,13 @@
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      ,@body))
 
-(defmacro defeasyclass( name (&rest direct-superclasses) (&rest members) )
+(defmacro defeasyclass( name (&rest direct-superclasses) (&rest members) &key (export-all t))
   (let ((sym-o (gensym))(sym-s (gensym)))
-    `(progn
-     (defclass ,name (,@direct-superclasses)
-	(,@(loop for member in members collecting `(,member :initarg ,(to-keyword member) :reader ,member))))
-     (defmethod print-object( (,sym-o ,name) ,sym-s) (format ,sym-s ,(apply #'concatenate 'string (loop for member in members collecting (% "{~a:~a}" (symbol-name member) "~a"))) ,@(loop for member in members collecting `(format-repl (,member ,sym-o)))))
-     (defun ,name ,members (make-instance (quote ,name) ,@(apply #'concatenate 'list (loop for member in members collecting `(,(to-keyword member) ,member))))))))
+    (let ((exports))
+      (when export-all (push `(export (list ,@(qmap (member) (list 'quote member) members) ,(list 'quote name))) exports))
+      `(progn
+	 (defclass ,name (,@direct-superclasses)
+	   (,@(loop for member in members collecting `(,member :initarg ,(to-keyword member) :reader ,member))))
+	 (defmethod print-object( (,sym-o ,name) ,sym-s) (format ,sym-s ,(apply #'concatenate 'string (loop for member in members collecting (% "{~a:~a}" (symbol-name member) "~a"))) ,@(loop for member in members collecting `(format-repl (,member ,sym-o)))))
+	 (defun ,name ,members (make-instance (quote ,name) ,@(apply #'concatenate 'list (loop for member in members collecting `(,(to-keyword member) ,member)))))
+	 ,@exports))))
